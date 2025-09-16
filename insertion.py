f8343ebd5102a1299f5a7b00a63fc3dcf88da928
son-2005 <tigranyan_2005@bk.ru>
2025-09-16 22:29:22 +0400

    Add files via upload
    insertion

---
+++ insertion.py
@@ -0,0 +1,12 @@
+def sort(a):
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j - 1
+        while i >= 0 and a[i] > key:
+            a[i+1] = a[i]
+            i -= 1
+        a[i+1] = key
+    return a
+
+num = [10, 64, 5, 12, 45]
+print(sort(num))
