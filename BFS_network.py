dd1aba3d614278eaa81d2370665abefe02efc2f5
son-2005 <tigranyan_2005@bk.ru>
2025-12-13 17:12:29 +0400

    Add files via upload

---
+++ BFS_network.py
@@ -0,0 +1,31 @@
+graph_network = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+def bfs(graph, start_node):
+    visited = {start_node}
+    queue = [start_node]
+    traversal_path = []
+
+    while queue:
+        g = queue.pop(0) 
+        
+        traversal_path.append(g)
+
+        for neighbor in graph_network[g]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)                
+    return traversal_path
+
+start = 'A'
+print("BFS-ի ճանապարհը (սկիզբը 'A'):")
+result = bfs(graph_network, start)
+print(" -> ".join(result))
+
+
